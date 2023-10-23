7d740df5c386df385f9cb0e86e9a4501a0be7c15
Joseph <iossif123gg@gmail.com>
2023-10-23 12:03:51 +0300

    styling boilerplate

--- apps/ui/src/app/app.tsx
+++ apps/ui/src/app/app.tsx
@@ -2,20 +2,40 @@ import { useState } from 'react';
 import { transformFunctionDeclaration } from '../api/api';
 
 export default function App() {
-  const [code, setCode] = useState(`function sum(a, b) {
-    return a + b;
-  }`);
+  const [input, setInput] = useState(`function sum(a, b) {
+  return a + b;
+}`);
+  const [code, setCode] = useState('');
 
   const click = () => {
-    transformFunctionDeclaration({ code }).then((data) => setCode(data.code));
+    transformFunctionDeclaration({ code: input }).then((data) =>
+      setCode(data.code)
+    );
   };
 
   return (
-    <div>
-      <button onClick={click}>Button</button>
-      <pre>
-        <code>{code}</code>
-      </pre>
+    <div className="flex">
+      <div>
+        <div className="flexCol">
+          <textarea
+            cols={30}
+            rows={8}
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          ></textarea>
+          <select>
+            <option value="1">Function Declaration to Arrow Function</option>
+            <option value="2">Arrow Function to Function Declaration</option>
+          </select>
+        </div>
+        <button onClick={click}>Button</button>
+      </div>
+
+      <div>
+        <pre>
+          <code>{code}</code>
+        </pre>
+      </div>
     </div>
   );
 }
